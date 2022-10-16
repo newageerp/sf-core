08ea8bfda45bdd984642af0639bced2447f5532c
Arturas Jegorenkovas <jegorenkov@gmail.com>
2022-10-16 11:27:39 +0000

    fix

--- SfReactTemplates/assets/v3/form/ro-fields/ArrayRoField.tsx
+++ SfReactTemplates/assets/v3/form/ro-fields/ArrayRoField.tsx
@@ -1,19 +1,30 @@
+import { functions, UI } from '@newageerp/nae-react-ui';
 import React, { Fragment } from 'react'
 import { useTemplateLoader } from '../../templates/TemplateLoader';
 
 interface Props {
   fieldKey: string;
+
+  tabSchema: string;
+  tabType: string;
 }
 
 export default function ArrayRoField(props: Props) {
   const { data: tData } = useTemplateLoader();
   const { element } = tData;
 
+  const value: any[] = element[props.fieldKey] ? element[props.fieldKey] : []
+
   if (!element) {
     return <Fragment />;
   }
 
   return (
-    <div>ArrayRoField</div>
+    <UI.Form.ArrayRo
+        title={""}
+        schema={props.tabSchema}
+        value={value}
+        tab={functions.tabs.getTabFromSchemaAndType(props.tabSchema, props.tabType)}
+      />
   )
 }
