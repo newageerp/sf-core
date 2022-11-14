bb85a2ffe21667f9973dc36a5afab6861ccc3a1c
Arturas Jegorenkovas <jegorenkov@gmail.com>
2022-11-14 07:34:48 +0000

    progress

--- SfReactTemplates/assets/v3/popup/PopupWindow.tsx
+++ SfReactTemplates/assets/v3/popup/PopupWindow.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { Template, TemplatesParser, useTemplateLoader } from '../templates/TemplateLoader'
 import { NaePopupProvider } from '../old-ui/OldPopupProvider';
 import { Popup } from '@newageerp/v3.popups.popup'
+
 interface Props {
   children: Template[],
-  // size?: PopupSize,
+  className?: string,
   title?: string,
 }
 
@@ -13,7 +14,7 @@ export default function PopupWindow(props: Props) {
 
   return (
     <NaePopupProvider isPopup={true} onClose={tdata.onBack}>
-      <Popup isPopup={true} onClick={tdata.onBack} title={props.title ? props.title : ''}>
+      <Popup className={props.className} isPopup={true} onClick={tdata.onBack} title={props.title ? props.title : ''}>
         <TemplatesParser templates={props.children} />
       </Popup>
     </NaePopupProvider>
