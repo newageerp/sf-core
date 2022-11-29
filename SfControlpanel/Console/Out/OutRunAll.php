c983eae06d7a8e1adbc941a9841f81251063378a
Arturas Jegorenkovas <jegorenkov@gmail.com>
2022-11-29 08:49:19 +0000

    progress

--- SfControlpanel/Console/Out/OutRunAll.php
+++ SfControlpanel/Console/Out/OutRunAll.php
@@ -5,6 +5,7 @@ namespace Newageerp\SfControlpanel\Console\Out;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class OutRunAll extends Command
@@ -29,12 +30,21 @@ class OutRunAll extends Command
         foreach ($commands as $commandName) {
             $output->writeln('Running command ' . $commandName);
 
-            $greetInput = new ArrayInput([]);
+            $input = new ArrayInput([
+                'command' => $commandName,
+                // '--format' => 'json',
+                // '-v'
+            ]);
 
-            $command = $this->getApplication()->find($commandName);
-            $returnCode = $command->run($greetInput, $output);
+            $output = new BufferedOutput(BufferedOutput::VERBOSITY_QUIET);
+            $this->getApplication()->run($input, $output);
 
-            $output->writeln('Finnish command ' . $commandName . ' with status code ' . $returnCode);
+            // $greetInput = new ArrayInput([]);
+
+            // $command = $this->getApplication()->find($commandName);
+            // $returnCode = $command->run($greetInput, $output);
+
+            $output->writeln('Finnish command ' . $commandName);
         }
 
         return Command::SUCCESS;
