1a0259befb3d389174fcba5d445eecbcb6e0ad84
Arturas Jegorenkovas <jegorenkov@gmail.com>
2022-12-05 06:46:23 +0000

    progress

---
+++ SfReactTemplates/CoreTemplates/MainMenu/MenuService.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Newageerp\SfReactTemplates\CoreTemplates\MainMenu;
+
+use Newageerp\SfControlpanel\Console\LocalConfigUtils;
+use Newageerp\SfReactTemplates\Template\Placeholder;
+
+class MenuService {
+    public function parseFolder(string $slug, Placeholder $placeholder)
+    {
+        $data = LocalConfigUtils::getCpConfigFileData('menu-cache');
+
+        foreach ($data as $item) {
+            if ($item['slug'] === $slug) {
+                if ($item['design'] === 'Virtual') {
+                    $this->parseVirtualFolderContent($item['Content'], $placeholder);
+                }
+            }
+        }
+    }
+
+    public function parseVirtualFolderContent(array $content, Placeholder $placeholder)
+    {
+        foreach ($content as $item) {
+            if ($item['__component'] === 'menu.folder') {
+                if ($item['menu_folder']['data']['attributes']['Design'] === 'Virtual') {
+                    $this->parseVirtualFolderContent(
+                        $item['menu_folder']['data']['attributes']['Content'],
+                        $placeholder
+                    );
+                } else {
+                    $placeholder->addTemplate(
+                        $this->folderFactory($item['menu_folder']['data']['attributes'], $item['menu_folder']['data']['id'])
+                    );
+                }
+            }
+            if ($item['__component'] === 'menu.title') {
+                $menuTitle = new MenuTitle($item['Title']);
+                $placeholder->addTemplate($menuTitle);
+            }
+            if ($item['__component'] === 'menu.menu-item-tab') {
+                $placeholder->addTemplate(
+                    $this->menuItemFactory->linkForTab(
+                        $item['Scheme'],
+                        $item['Type'],
+                        $item['Icon']
+                    )
+                );
+            }
+        }
+    }
+
+    public function folderFactory(array $data, ?int $id): MenuFolder
+    {
+        $folder = new MenuFolder(
+            $data['Title'],
+            $data['Icon'],
+        );
+        $folder->setMenuFolderId('folder-'.$id);
+
+        foreach ($data['Content'] as $item) {
+            if ($item['__component'] === 'menu.menu-item-tab') {
+                $folder->addItem(
+                    $this->menuItemFactory->linkForTab(
+                        $item['Scheme'],
+                        $item['Type'],
+                        $item['Icon']
+                    )
+                );
+            }
+            if ($item['__component'] === 'menu.divider') {
+                $folder->addItem(
+                    new MenuDivider()
+                );
+            }
+            if ($item['__component'] === 'menu.folder') {
+                $subFolder = $this->folderFactory($item['menu_folder']['data']['attributes'], $item['menu_folder']['data']['id']);
+                $subFolder->setContentClassName('tw3-pl-4');
+                $folder->addItem($subFolder);
+            }
+        }
+
+        return $folder;
+    }
+}
