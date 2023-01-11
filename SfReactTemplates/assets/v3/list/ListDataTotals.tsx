94f193e97aea2465d11311fff1ecf22d02dbe7f7
Arturas Jegorenkovas <jegorenkov@gmail.com>
2023-01-11 16:32:58 +0000

    progress

--- SfReactTemplates/assets/v3/list/ListDataTotals.tsx
+++ SfReactTemplates/assets/v3/list/ListDataTotals.tsx
@@ -17,7 +17,7 @@ export default function ListDataTotals(props: Props) {
                     <div className="tw3-flex tw3-items-center tw3-justify-end">
                         <label className="tw3-text-sm tw3-font-semibold tw3-text-right tw3-w-36 tw3-p-2 tw3-bg-white">{total.title}</label>
                         {!!tData.dataTotals && (total.field in tData.dataTotals) &&
-                            <span className="tw3-w-36 tw3-text-right tw3-text-sm p-2 tw3-bg-white">{tData.dataTotals[total.field].toFixed(2)}</span>
+                            <span className="tw3-w-36 tw3-text-right tw3-text-sm tw3-p-2 tw3-bg-white">{tData.dataTotals[total.field].toFixed(2)}</span>
                         }
                     </div>
                 )
