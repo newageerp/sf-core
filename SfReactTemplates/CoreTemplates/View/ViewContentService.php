d995f5108dfce85c26dd6edd52e329cb022fa609
Arturas Jegorenkovas <jegorenkov@gmail.com>
2023-02-24 14:55:14 +0000

    progress

--- SfReactTemplates/CoreTemplates/View/ViewContentService.php
+++ SfReactTemplates/CoreTemplates/View/ViewContentService.php
@@ -154,7 +154,7 @@ class ViewContentService
                         }
                         $wideRow->getLabelContent()->addTemplate($label);
                     } else if ($isCompact) {
-                        $label = new FormFieldLabel(" ");
+                        $label = new FormFieldLabel(" ");
                         $label->setWidth('tw3-w-full');
                         $wideRow->getLabelContent()->addTemplate($label);
                     }
