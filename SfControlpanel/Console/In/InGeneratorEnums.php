772cc63c626490c9986d6523cce4f96bfbc1d20f
Arturas Jegorenkovas <jegorenkov@gmail.com>
2023-03-22 08:08:00 +0000

    progress

--- SfControlpanel/Console/In/InGeneratorEnums.php
+++ SfControlpanel/Console/In/InGeneratorEnums.php
@@ -24,62 +24,62 @@ class InGeneratorEnums extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $statusItemsTemplate = file_get_contents(
-            __DIR__ . '/templates/enums/EnumItems.txt'
-        );
+        // $statusItemsTemplate = file_get_contents(
+        //     __DIR__ . '/templates/enums/EnumItems.txt'
+        // );
 
-        $generatedPath = Utils::generatedPath('enums/view');
+        // $generatedPath = Utils::generatedPath('enums/view');
 
-        $enumsList = LocalConfigUtils::getCpConfigFileData('enums');
+        // $enumsList = LocalConfigUtils::getCpConfigFileData('enums');
 
-        $enums = [];
-        $enumsColors = [];
-        $enumsOptions = [];
-        foreach ($enumsList as $enum) {
-            if (!isset($enums[$enum['config']['entity']])) {
-                $enums[$enum['config']['entity']] = [];
-                $enumsColors[$enum['config']['entity']] = [];
-                $enumsOptions[$enum['config']['entity']] = [];
-            }
-            if (!isset($enums[$enum['config']['entity']][$enum['config']['property']])) {
-                $enums[$enum['config']['entity']][$enum['config']['property']] = [];
-                $enumsColors[$enum['config']['entity']][$enum['config']['property']] = [];
-                $enumsOptions[$enum['config']['entity']][$enum['config']['property']] = [];
-            }
-            $enumsOptions[$enum['config']['entity']][$enum['config']['property']][] = [
-                'value' => $enum['config']['value'],
-                'label' => $enum['config']['title'],
-            ];
-            $enums[$enum['config']['entity']][$enum['config']['property']][$enum['config']['value']] = $enum['config']['title'];
-            $enumsColors[$enum['config']['entity']][$enum['config']['property']][$enum['config']['value']] = isset($enum['config']['badgeVariant']) && $enum['config']['badgeVariant']?$enum['config']['badgeVariant']:'slate-50';
-        }
+        // $enums = [];
+        // $enumsColors = [];
+        // $enumsOptions = [];
+        // foreach ($enumsList as $enum) {
+        //     if (!isset($enums[$enum['config']['entity']])) {
+        //         $enums[$enum['config']['entity']] = [];
+        //         $enumsColors[$enum['config']['entity']] = [];
+        //         $enumsOptions[$enum['config']['entity']] = [];
+        //     }
+        //     if (!isset($enums[$enum['config']['entity']][$enum['config']['property']])) {
+        //         $enums[$enum['config']['entity']][$enum['config']['property']] = [];
+        //         $enumsColors[$enum['config']['entity']][$enum['config']['property']] = [];
+        //         $enumsOptions[$enum['config']['entity']][$enum['config']['property']] = [];
+        //     }
+        //     $enumsOptions[$enum['config']['entity']][$enum['config']['property']][] = [
+        //         'value' => $enum['config']['value'],
+        //         'label' => $enum['config']['title'],
+        //     ];
+        //     $enums[$enum['config']['entity']][$enum['config']['property']][$enum['config']['value']] = $enum['config']['title'];
+        //     $enumsColors[$enum['config']['entity']][$enum['config']['property']][$enum['config']['value']] = isset($enum['config']['badgeVariant']) && $enum['config']['badgeVariant']?$enum['config']['badgeVariant']:'slate-50';
+        // }
 
-        foreach ($enums as $slug => $propertyKeys) {
-            $compName = Utils::fixComponentName(ucfirst($slug) . 'Enums');
+        // foreach ($enums as $slug => $propertyKeys) {
+        //     $compName = Utils::fixComponentName(ucfirst($slug) . 'Enums');
 
-            $tpEnumStr = json_encode($propertyKeys, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-            $tpEnumColorsStr = json_encode($enumsColors[$slug], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-            $tpEnumOptionsStr = json_encode($enumsOptions[$slug], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        //     $tpEnumStr = json_encode($propertyKeys, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        //     $tpEnumColorsStr = json_encode($enumsColors[$slug], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        //     $tpEnumOptionsStr = json_encode($enumsOptions[$slug], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 
-            $fileName = $generatedPath . '/' . $compName . '.tsx';
-            $generatedContent = str_replace(
-                [
-                    'TP_ENUMS_COLORS',
-                    'TP_ENUMS_OPTIONS',
-                    'TP_ENUMS',
-                    'TP_COMP_NAME',
-                ],
-                [
-                    $tpEnumColorsStr,
-                    $tpEnumOptionsStr,
-                    $tpEnumStr,
-                    $compName,
-                ],
-                $statusItemsTemplate
-            );
-            Utils::writeOnChanges($fileName, $generatedContent);
+        //     $fileName = $generatedPath . '/' . $compName . '.tsx';
+        //     $generatedContent = str_replace(
+        //         [
+        //             'TP_ENUMS_COLORS',
+        //             'TP_ENUMS_OPTIONS',
+        //             'TP_ENUMS',
+        //             'TP_COMP_NAME',
+        //         ],
+        //         [
+        //             $tpEnumColorsStr,
+        //             $tpEnumOptionsStr,
+        //             $tpEnumStr,
+        //             $compName,
+        //         ],
+        //         $statusItemsTemplate
+        //     );
+        //     Utils::writeOnChanges($fileName, $generatedContent);
 
-        }
+        // }
 
         return Command::SUCCESS;
     }
--- SfControlpanel/Console/In/InGeneratorStatuses.php
+++ SfControlpanel/Console/In/InGeneratorStatuses.php
@@ -24,84 +24,84 @@ class InGeneratorStatuses extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__, 2) . '/templates');
-        $twig = new \Twig\Environment($loader, [
-            'cache' => '/tmp',
-        ]);
+        // $loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__, 2) . '/templates');
+        // $twig = new \Twig\Environment($loader, [
+        //     'cache' => '/tmp',
+        // ]);
 
-        $statusItemsTemplate = $twig->load('statuses/StatusItems.html.twig');
+        // $statusItemsTemplate = $twig->load('statuses/StatusItems.html.twig');
 
 
-        $generatedPath = Utils::generatedPath('statuses/badges');
+        // $generatedPath = Utils::generatedPath('statuses/badges');
 
-        $statusData = LocalConfigUtils::getCpConfigFileData('statuses');
+        // $statusData = LocalConfigUtils::getCpConfigFileData('statuses');
 
-        $statuses = [];
-        $statusJson = [];
-        $statusCompJson = [];
+        // $statuses = [];
+        // $statusJson = [];
+        // $statusCompJson = [];
 
-        foreach ($statusData as $status) {
-            $entity = $status['config']['entity'];
-            $type = $status['config']['type'];
-            $typeUc = Utils::fixComponentName($status['config']['type']);
+        // foreach ($statusData as $status) {
+        //     $entity = $status['config']['entity'];
+        //     $type = $status['config']['type'];
+        //     $typeUc = Utils::fixComponentName($status['config']['type']);
 
-            if (!isset($statuses[$entity])) {
-                $statuses[$entity] = [];
-            }
-            $statuses[$entity][] = $status;
+        //     if (!isset($statuses[$entity])) {
+        //         $statuses[$entity] = [];
+        //     }
+        //     $statuses[$entity][] = $status;
 
-            if (!isset($statusJson[$entity])) {
-                $statusJson[$entity] = [];
-                $statusCompJson[$entity] = [];
-            }
-            if (!isset($statusJson[$entity][$type])) {
-                $statusJson[$entity][$type] = [];
-            }
-            if (!isset($statusJson[$entity][$typeUc])) {
-                $statusCompJson[$entity][$typeUc] = [
-                    'type' => $type,
-                ];
-            }
-        }
+        //     if (!isset($statusJson[$entity])) {
+        //         $statusJson[$entity] = [];
+        //         $statusCompJson[$entity] = [];
+        //     }
+        //     if (!isset($statusJson[$entity][$type])) {
+        //         $statusJson[$entity][$type] = [];
+        //     }
+        //     if (!isset($statusJson[$entity][$typeUc])) {
+        //         $statusCompJson[$entity][$typeUc] = [
+        //             'type' => $type,
+        //         ];
+        //     }
+        // }
 
-        foreach ($statuses as $slug => $entityStatuses) {
-            $statusData = [];
+        // foreach ($statuses as $slug => $entityStatuses) {
+        //     $statusData = [];
 
-            $compName = Utils::fixComponentName(ucfirst($slug) . 'Statuses');
+        //     $compName = Utils::fixComponentName(ucfirst($slug) . 'Statuses');
 
-            foreach ($entityStatuses as $status) {
-                $typeUc = Utils::fixComponentName($status['config']['type']);
+        //     foreach ($entityStatuses as $status) {
+        //         $typeUc = Utils::fixComponentName($status['config']['type']);
 
-                $statusName = Utils::fixComponentName(
-                    ucfirst($slug) .ucfirst($status['config']['type']) . 'Badge' . $status['config']['status']
-                );
-                $el = [
-                    'statusName' => $statusName,
-                    'color' => $status['config']['color'],
-                    'brightness' => mb_substr($status['config']['brightness'], 1),
-                    'text' => $status['config']['text'],
-                    'status' => (int)$status['config']['status'],
-                    'type' => $status['config']['type'],
-                    'bgColor' => isset($status['config']['badgeVariant']) ? $status['config']['badgeVariant'] : ''
-                ];
-                $statusData[] = $el;
-                $statusJson[$slug][$status['config']['type']][] = $el;
-            }
+        //         $statusName = Utils::fixComponentName(
+        //             ucfirst($slug) .ucfirst($status['config']['type']) . 'Badge' . $status['config']['status']
+        //         );
+        //         $el = [
+        //             'statusName' => $statusName,
+        //             'color' => $status['config']['color'],
+        //             'brightness' => mb_substr($status['config']['brightness'], 1),
+        //             'text' => $status['config']['text'],
+        //             'status' => (int)$status['config']['status'],
+        //             'type' => $status['config']['type'],
+        //             'bgColor' => isset($status['config']['badgeVariant']) ? $status['config']['badgeVariant'] : ''
+        //         ];
+        //         $statusData[] = $el;
+        //         $statusJson[$slug][$status['config']['type']][] = $el;
+        //     }
 
-            $fileName = $generatedPath . '/' . $compName . '.tsx';
+        //     $fileName = $generatedPath . '/' . $compName . '.tsx';
 
-            $generatedContent = $statusItemsTemplate->render(
-                [
-                    'TP_COMP_NAME' => $compName,
-                    'statusData' => $statusData,
-                    'statusJson' => json_encode($statusJson[$slug], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE),
-                    'statusCompJson' => $statusCompJson[$slug],
-                    'schema' => $slug,
-                    'schemaUc' => Utils::fixComponentName($slug)
-                ]
-            );
-            Utils::writeOnChanges($fileName, $generatedContent);
-        }
+        //     $generatedContent = $statusItemsTemplate->render(
+        //         [
+        //             'TP_COMP_NAME' => $compName,
+        //             'statusData' => $statusData,
+        //             'statusJson' => json_encode($statusJson[$slug], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE),
+        //             'statusCompJson' => $statusCompJson[$slug],
+        //             'schema' => $slug,
+        //             'schemaUc' => Utils::fixComponentName($slug)
+        //         ]
+        //     );
+        //     Utils::writeOnChanges($fileName, $generatedContent);
+        // }
 
         return Command::SUCCESS;
     }
