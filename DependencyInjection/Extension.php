de329ec7746c5d72d63875754f19bf3dbe0ea894
Arturas Jegorenkovas <jegorenkov@gmail.com>
2023-07-14 10:55:14 +0000

    monolog config

--- DependencyInjection/Extension.php
+++ DependencyInjection/Extension.php
@@ -24,10 +24,6 @@ class Extension extends ConfigurableExtension
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../config'));
         $loader->load('services.yaml');
-
-        
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../config'));
-        $loader->load('monolog.yaml');
     }
 
 
