f88aa23d1cab93bd3bf3164eeb5c87be03749d1a
Arturas Jegorenkovas <jegorenkov@gmail.com>
2023-07-14 17:25:30 +0000

    monolog config

--- SfShedulledTask/SfShedulledTask.php
+++ SfShedulledTask/SfShedulledTask.php
@@ -1,9 +1,28 @@
 <?php
+
 namespace Newageerp\SfShedulledTask;
 
+use Psr\Log\LoggerInterface;
 use Rewieer\TaskSchedulerBundle\Task\AbstractScheduledTask;
 
 abstract class SfShedulledTask extends AbstractScheduledTask
 {
+    protected LoggerInterface $taskLogger;
 
+    public function __construct(LoggerInterface $taskLogger)
+    {
+        parent::__construct();
+
+        $this->taskLogger = $taskLogger;
+    }
+
+    abstract public function runWithTimer(): void;
+
+    public function run(): void
+    {
+        $timeStart = microtime(true);
+        $this->runWithTimer();
+        $timeFinish = microtime(true);
+        $this->taskLogger->info('Run task ' . $this::class . ' ' . number_format($timeFinish - $timeStart, 5));
+    }
 }
