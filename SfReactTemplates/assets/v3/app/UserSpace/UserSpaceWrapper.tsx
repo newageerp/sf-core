0ba162bed723fd72b671a7c9efd190c7e5cdc34a
Arturas Jegorenkovas <jegorenkov@gmail.com>
2023-08-18 13:50:57 +0000

    templates

--- SfReactTemplates/assets/v3/app/UserSpace/UserSpaceWrapper.tsx
+++ SfReactTemplates/assets/v3/app/UserSpace/UserSpaceWrapper.tsx
@@ -38,39 +38,3 @@ function UserSpaceWrapper(props: Props) {
 }
 
 export default UserSpaceWrapper;
-
-
-interface Props {
-  children?: any;
-}
-
-function UserSpaceWrapper(props: Props) {
-  const { data: tData } = useTemplatesLoader();
-  const isDev = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development');
-
-  return (
-    <MainPage
-      sideBar={
-        <Fragment>
-          <div>
-            <TemplatesParser
-              templates={tData.userSpaceWrapperLeft}
-            />
-          </div>
-
-          {isDev &&
-            <div>
-              <a className="tw3-text-white tw3-mt-20 tw3-text-xs tw3-underline tw3-decoration-wavy" href="/app/nae-core/config-menu/regenerate" target="_blank">Regenerate menu</a>
-            </div>
-          }
-          <DataCacheSocketComponent />
-        </Fragment>
-      }
-      header={<UserHeader />}
-    >
-      {props.children}
-    </MainPage>
-  )
-}
-
-export default UserSpaceWrapper;
