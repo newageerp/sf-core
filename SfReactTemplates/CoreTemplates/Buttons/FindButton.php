947faf354ae1a3496f59f7f33a76fa483032ea44
Arturas Jegorenkovas <info@newageerp.com>
2024-04-29 19:33:48 +0300

    Find button

--- SfReactTemplates/CoreTemplates/Buttons/FindButton.php
+++ SfReactTemplates/CoreTemplates/Buttons/FindButton.php
@@ -35,7 +35,7 @@ class FindButton extends Template
 
     public function getTemplateName(): string
     {
-        return '_.ButtonsBundle.FindButton';
+        return 'list.table.find-button';
     }
 
     /**
